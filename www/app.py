32ef04d923d3c22ff5408ddf29903353d2f86a10
zeatual <zeatual@126.com>
2019-10-03 20:36:12 +0800

    day2 finished

--- www/app.py
+++ www/app.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+
+async def index(request):
+    return web.Response(text='初步搭建框架！')
+
+app=web.Application()
+app.add_routes([web.get('/',index)])
+web.run_app(app,host='127.0.0.1',port=9090)
+
